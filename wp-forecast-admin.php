9e79e6df6ddec71bf117b7558d9bea3a8874214a
tuxlog <tuxlog@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-23 11:01:59 +0000

    wp-forecast v4.2 trunk
    git-svn-id: https://plugins.svn.wordpress.org/wp-forecast/trunk@616100 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-forecast-admin.php
+++ wp-forecast-admin.php
@@ -562,6 +562,7 @@ function wpf_sub_admin_form($wpfcid,$widgetcall) {
             <option value="fr_FR" <?php if ($av['wpf_language']=="fr_FR") echo "selected=\"selected\""?>>french</option>
             <option value="he_IL" <?php if ($av['wpf_language']=="he_IL") echo "selected=\"selected\""?>>hebrew</option>
             <option value="hu_HU" <?php if ($av['wpf_language']=="hu_HU") echo "selected=\"selected\""?>>hungarian</option>
+            <option value="id_ID" <?php if ($av['wpf_language']=="id_ID") echo "selected=\"selected\""?>>indonesian</option>
             <option value="it_IT" <?php if ($av['wpf_language']=="it_IT") echo "selected=\"selected\""?>>italian</option>
 	    <option value="pl_PL" <?php if ($av['wpf_language']=="pl_PL") echo "selected=\"selected\""?>>polish</option>
             <option value="pt_PT" <?php if ($av['wpf_language']=="pt_PT") echo "selected=\"selected\""?>>portugu&#234;s</option>
